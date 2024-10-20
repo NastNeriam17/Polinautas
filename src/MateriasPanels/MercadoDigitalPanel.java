db32c8af160e3d285da2eafd785aa73eb4bfc8da
DR4G4NS <68481728+TheDragonsCrafts@users.noreply.github.com>
2024-10-19 19:10:01 -0600

    cam. menores

--- src/MateriasPanels/MercadoDigitalPanel.java
+++ src/MateriasPanels/MercadoDigitalPanel.java
@@ -4,6 +4,8 @@
  */
 package MateriasPanels;
 
+import Formularios.MainPage;
+
 /**
  *
  * @author IanDa
@@ -26,19 +28,48 @@ public class MercadoDigitalPanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jButton1 = new javax.swing.JButton();
+
+        jButton1.setText("Regresar a menú");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 154, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(240, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(230, Short.MAX_VALUE)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(20, 20, 20))
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // toma una referencia del MainPage frame
+    MainPage mainFrame = (MainPage) javax.swing.SwingUtilities.getWindowAncestor(this);
+
+    //Checa si el ancestro es en efecto, el MainPage Frame
+    if (mainFrame != null) {
+        mainFrame.menuPrincipal();
+    } else {
+        // toma la excepcion si el ancestro no es encontrado
+        System.err.println("MainPage frame no encontrado.");
+    }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
     // End of variables declaration//GEN-END:variables
 }
